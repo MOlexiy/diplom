bba7e9d46fbafa979bfb07d1afc96a6ea157154c
MOlexiy <morm.alyosha@gmail.com>
2022-05-06 11:35:57 +0300

    06/05/2022

--- Predict.py
+++ Predict.py
@@ -1,5 +1,3 @@
-import tkinter
-
 import matplotlib
 import numpy as np
 from datetime import datetime
@@ -8,17 +6,10 @@ import random
 from tkinter import *
 from tkinter.ttk import Combobox
 from statsmodels.tsa.arima.model import ARIMA
-from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing, Holt
-import pmdarima as pm
+from statsmodels.tsa.api import ExponentialSmoothing
 from sklearn.metrics import mean_squared_error
 from math import sqrt
-from statsmodels.tsa.arima.model import ARIMAResults
-import imgkit
-import PIL
-import seaborn as sns
 import pandas as pd
-import matplotlib.pyplot as plt
-import csv
 from sklearn.preprocessing import PolynomialFeatures
 from bokeh.plotting import figure, show, output_file
 from openpyxl import load_workbook
